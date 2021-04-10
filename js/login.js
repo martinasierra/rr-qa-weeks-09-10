1ccfbeedca4ec888534cdddef188c2bd66b3ec75
Martina Sierra <martinasierra94@gmail.com>
2021-04-09 23:45:52 -0300

    Restructure validations

--- js/login.js
+++ js/login.js
@@ -1,47 +1,85 @@
-function validate() {
-    let hasForm = document.getElementsByTagName('form');
-    let hasInputs = document.getElementsByTagName('input');
-    let isRequiredE = document.querySelector(".reqE").required;
-    let isRequiredP = document.querySelector(".reqP").required;
-    const a = document.querySelector('a');
-    a.hasAttribute('href');
-    let valueHref = a.getAttribute('href');
-    const buttonSignIn = document.querySelector('.button');
-    buttonSignIn.hasAttribute('value');
-    let valueButton = buttonSignIn.getAttribute('value');
-    if (hasForm.length<1) { 
-        var node = document.createElement("li");                 
-        var textnode = document.createTextNode('Validations results: form is not found');         
+var myList = document.querySelector('.myList');
+var validationDiv = document.querySelector('.validationDiv')
+
+function formExists() {
+let hasForm = document.getElementsByTagName('form');
+if (hasForm.length<1) { 
+        var node = document.createElement("li");                
+        var textnode = document.createTextNode('Form is not found');         
         node.appendChild(textnode);                             
-        document.getElementById("myList").appendChild(node); 
-}   else if (hasInputs.length<3) {
+        myList.appendChild(node); 
+} else {
+        return true;
+} }
+
+function inputQuantity() {
+let hasInputs = document.getElementsByTagName('input');
+if (hasInputs.length<3) {
         var node = document.createElement("li");                 
         var textnode = document.createTextNode('Missing input fields');         
         node.appendChild(textnode);                             
-        document.getElementById("myList").appendChild(node); 
-}   else if (isRequiredE === false) {
+        myList.appendChild(node); 
+} else {
+        return true;
+} }
+
+function emailRequired() {
+let isRequiredE = document.querySelector(".reqE").required;
+if (isRequiredE === false) {
         var node = document.createElement("li");                 
         var textnode = document.createTextNode('Email must be required');         
         node.appendChild(textnode);                             
-        document.getElementById("myList").appendChild(node); 
-}   else if (isRequiredP === false) {
+        myList.appendChild(node); 
+} else {
+        return true; 
+} }
+
+function passRequired() {
+let isRequiredP = document.querySelector(".reqP").required;
+if (isRequiredP === false) {
         var node = document.createElement("li");                 
         var textnode = document.createTextNode('Password must be required');         
         node.appendChild(textnode);                             
-        document.getElementById("myList").appendChild(node); 
-}   else if (valueHref !== 'register.html'){
+        myList.appendChild(node); 
+} else {
+        return true;
+} }
+
+function hrefValueRegister() {
+const a = document.querySelector('a');
+a.hasAttribute('href');
+let valueHref = a.getAttribute('href');
+if (valueHref !== 'register.html') {
         var node = document.createElement("li");                 
         var textnode = document.createTextNode('Incorrect href value');         
         node.appendChild(textnode);                             
-        document.getElementById("myList").appendChild(node);
-}   else if (valueButton !== 'Sign In') {
+        myList.appendChild(node);    
+} else {
+return true;
+} }
+
+function buttonContent() {
+const signInButton = document.querySelector('.button');
+signInButton.hasAttribute('value');
+let valueButton = signInButton.getAttribute('value');
+if (valueButton !== 'Sign In') {
         var node = document.createElement("li");                 
         var textnode = document.createTextNode('Incorrect button content');         
         node.appendChild(textnode);                             
-        document.getElementById("myList").appendChild(node);  
-}   else {
-    var node = document.createElement("li");                 
-    var textnode = document.createTextNode('Validations results: every validation has passed');         
-    node.appendChild(textnode);                             
-    document.getElementById("myList").appendChild(node); 
-}  }
+        myList.appendChild(node);  
+} else {
+        return true; 
+} }
+
+function validate() {
+if ((formExists()===true) && (inputQuantity()===true) && (emailRequired()===true) && (passRequired()===true) && (hrefValueRegister()===true) && (buttonContent()===true)){
+        var node = document.createElement("p");                 
+        var textnode = document.createTextNode('Validations results: every validation has passed');         
+        node.appendChild(textnode);                             
+        validationDiv.appendChild(node);
+} }
+
+
+
+
+
--- js/register.js
+++ js/register.js
@@ -1,59 +1,103 @@
-function validate() {
-    let hasForm = document.getElementsByTagName('form');
-    let hasInputs = document.getElementsByTagName('input');
-    let isRequiredE = document.querySelector(".reqE").required;
-    let isRequiredP = document.querySelector(".reqP").required;
-    let isRequiredFN = document.querySelector(".reqFN").required;
-    let isRequiredCPass = document.querySelector(".reqConfPass").required;
-    const buttonReset = document.querySelector('.b-reset');
-    buttonReset.hasAttribute('value');
-    let valueBReset = buttonReset.getAttribute('value');
-    const buttonSubmit = document.querySelector('.b-submit');
-    buttonSubmit.hasAttribute('value');
-    let valueBSubmit = buttonSubmit.getAttribute('value');
-    if (hasForm.length<1) { 
-        var node = document.createElement("li");                 
-        var textnode = document.createTextNode('Validations results: form is not found');         
+var myList = document.querySelector('.myList');
+var validationDiv = document.querySelector('.validationDiv')
+
+function formExists() {
+let hasForm = document.getElementsByTagName('form');
+if (hasForm.length<1) { 
+        var node = document.createElement("li");                
+        var textnode = document.createTextNode('Form is not found');         
         node.appendChild(textnode);                             
-        document.getElementById("myList").appendChild(node); 
-}   else if (hasInputs.length<6) {
+        myList.appendChild(node); 
+} else {
+        return true;
+} }
+        
+function inputQuantity() {
+let hasInputs = document.getElementsByTagName('input');
+if (hasInputs.length<3) {
         var node = document.createElement("li");                 
         var textnode = document.createTextNode('Missing input fields');         
         node.appendChild(textnode);                             
-        document.getElementById("myList").appendChild(node); 
-}   else if (isRequiredE === false) {
+        myList.appendChild(node); 
+} else {
+        return true;
+} }
+        
+function emailRequired() {
+let isRequiredE = document.querySelector(".reqE").required;
+if (isRequiredE === false) {
         var node = document.createElement("li");                 
         var textnode = document.createTextNode('Email must be required');         
         node.appendChild(textnode);                             
-        document.getElementById("myList").appendChild(node); 
-}   else if (isRequiredFN === false) {
+        myList.appendChild(node); 
+} else {
+        return true; 
+} }
+
+function fullNameRequired() {
+let isRequiredFN = document.querySelector(".reqFN").required;   
+if (isRequiredFN === false) {
         var node = document.createElement("li");                 
         var textnode = document.createTextNode('Full Name must be required');         
         node.appendChild(textnode);                             
         document.getElementById("myList").appendChild(node); 
-}   else if (isRequiredCPass === false) {
+} else {
+        return true; 
+} }
+
+function passRequired() {
+let isRequiredP = document.querySelector(".reqP").required;
+if (isRequiredP === false) {
+        var node = document.createElement("li");                 
+        var textnode = document.createTextNode('Password must be required');         
+        node.appendChild(textnode);                             
+        myList.appendChild(node); 
+} else {
+        return true;
+} }
+
+function confirmPass() {
+let isRequiredCPass = document.querySelector(".reqConfPass").required;
+if (isRequiredCPass === false) {
         var node = document.createElement("li");                 
         var textnode = document.createTextNode('Password confirmation must be required');         
         node.appendChild(textnode);                             
         document.getElementById("myList").appendChild(node); 
-}   else if (isRequiredP === false) {
+} else {
+        return true;
+} }
+
+function resetButtonContent() {
+const buttonReset = document.querySelector('.b-reset');
+buttonReset.hasAttribute('value');
+let valueBReset = buttonReset.getAttribute('value');
+if (valueBReset !== 'Reset') {
         var node = document.createElement("li");                 
-        var textnode = document.createTextNode('Password must be required');         
+        var textnode = document.createTextNode('Incorrect Reset button content');         
         node.appendChild(textnode);                             
-        document.getElementById("myList").appendChild(node); 
-}   else if (valueBSubmit !== 'Submit') {
+        document.getElementById("myList").appendChild(node);
+} else {
+        return true;
+} }
+
+function submitButtonContent() {
+const buttonSubmit = document.querySelector('.b-submit');
+buttonSubmit.hasAttribute('value');
+let valueBSubmit = buttonSubmit.getAttribute('value');
+if (valueBSubmit !== 'Submit') {
         var node = document.createElement("li");                 
         var textnode = document.createTextNode('Incorrect Submit button content');         
         node.appendChild(textnode);                             
         document.getElementById("myList").appendChild(node);  
-}   else if (valueBReset !== 'Reset') {
-        var node = document.createElement("li");                 
-        var textnode = document.createTextNode('Incorrect Reset button content');         
-        node.appendChild(textnode);                             
-        document.getElementById("myList").appendChild(node);  
-}   else {
-        var node = document.createElement("li");                 
+} else {
+        return true;
+} }
+
+function validate() {
+if ((formExists()===true) && (inputQuantity()===true) && (emailRequired()===true) && (fullNameRequired()===true) && (passRequired()===true) && (confirmPass()===true) && (resetButtonContent()===true) && (submitButtonContent()===true)){
+        var node = document.createElement("p");                 
         var textnode = document.createTextNode('Validations results: every validation has passed');         
         node.appendChild(textnode);                             
-        document.getElementById("myList").appendChild(node); 
-}  }
+        validationDiv.appendChild(node);
+} }
+                        
